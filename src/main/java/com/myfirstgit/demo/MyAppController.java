a5efe63d3ba236a81e7bdf9ea512ad9f78480830
shivsuhagiya <shivlalsuhagiya@gmail.com>
2022-04-12 18:37:39 +0530

    Initial commit

---
+++ src/main/java/com/myfirstgit/demo/MyAppController.java
@@ -0,0 +1,13 @@
+package com.myfirstgit.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyAppController {
+
+    @GetMapping("/mywelcome-message")
+    public String welcomeMessage(){
+        return "my first welcome message";
+    }
+}
---
+++ src/main/java/com/myfirstgit/demo/MyfirstgitdemoApplication.java
@@ -0,0 +1,13 @@
+package com.myfirstgit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstgitdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyfirstgitdemoApplication.class, args);
+    }
+
+}
