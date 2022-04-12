c9f21a4bb825c70d83d6e42f1038a08a450deeab
shivsuhagiya <shivlalsuhagiya@gmail.com>
2022-04-12 18:39:03 +0530

    first commit

--- src/main/java/com/myfirstgit/demo/MyAppController.java
+++ src/main/java/com/myfirstgit/demo/MyAppController.java
@@ -8,6 +8,6 @@ public class MyAppController {
 
     @GetMapping("/mywelcome-message")
     public String welcomeMessage(){
-        return "my first welcome message";
+        return "my first welcome message, my first commit successfully completed.";
     }
 }
